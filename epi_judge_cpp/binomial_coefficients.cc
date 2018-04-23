7a8940cd08c9e906137964601977267e985ba5f9
Sourabh Daptardar <saurabh.daptardar@gmail.com>
2018-04-23 17:56:01 +0530

    17.3: DP: Binomial co-efficients

--- epi_judge_cpp/binomial_coefficients.cc
+++ epi_judge_cpp/binomial_coefficients.cc
@@ -1,8 +1,20 @@
 #include "test_framework/generic_test.h"
+#include <vector>
+
+using std::min;
+using std::vector;
+using namespace std;
 
 int ComputeBinomialCoefficient(int n, int k) {
-  // Implement this placeholder.
-  return 0;
+  vector<int> A(k+1, 0);
+  A[0] = 1;
+  for (int i = 1 ; i <= n ; i++) {
+    for (int j = min(i, k) ; j >= 1 ; --j) {
+      A[j] = A[j] + A[j - 1];
+    }
+    A[0] = 1;
+  }
+  return A[k];
 }
 
 int main(int argc, char* argv[]) {
